60a28076ab353cba94449bd9fde6c685d2e20b77
s7s-khawaji <sehs.s.h@gmail.com>
2026-07-19 15:46:48 +0300

    Create BrokerRegister.jsx

---
+++ src/pages/BrokerRegister.jsx
@@ -0,0 +1,26 @@
+export default function BrokerRegister(){
+
+return(
+<div className="p-6">
+
+<h1 className="text-2xl font-bold mb-4">
+تسجيل وسيط عقاري
+</h1>
+
+<input className="border p-3 w-full mb-3" placeholder="الاسم"/>
+
+<input className="border p-3 w-full mb-3" placeholder="رقم الهاتف"/>
+
+<input className="border p-3 w-full mb-3" placeholder="البريد الإلكتروني"/>
+
+<input className="border p-3 w-full mb-3" placeholder="مناطق العمل"/>
+
+
+<button className="bg-blue-600 text-white p-3 rounded w-full">
+إنشاء حساب وسيط
+</button>
+
+</div>
+)
+
+}
