8b52aec041083ac23a6b5552b1fec302102b3373
s7s-khawaji <sehs.s.h@gmail.com>
2026-07-19 15:46:02 +0300

    Create AdminDashboard.jsx

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,51 @@
+export default function AdminDashboard(){
+
+return(
+
+<div className="p-6">
+
+<h1 className="text-2xl font-bold mb-5">
+لوحة الإدارة
+</h1>
+
+
+<div className="grid gap-4">
+
+<div className="border p-4 rounded">
+طلبات:
+100
+</div>
+
+
+<div className="border p-4 rounded">
+الوسطاء:
+50
+</div>
+
+
+<div className="border p-4 rounded">
+Lead-to-Offer Rate:
+35%
+</div>
+
+
+<div className="border p-4 rounded">
+مصادر العملاء:
+<br/>
+Facebook
+<br/>
+Google
+<br/>
+TikTok
+<br/>
+Referral
+</div>
+
+
+</div>
+
+</div>
+
+)
+
+}
